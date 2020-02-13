7df5a1e50220bb8d329163b7cc6e544c4afa9932
ConnorSheremeta <thecon21@gmail.com>
2020-02-13 02:17:27 -0700

    guards now become incapacitated, fixed null bug, start position of guard 1 moved right 1

--- HITDAD/js/plugins/SPFCore.js
+++ HITDAD/js/plugins/SPFCore.js
@@ -58,8 +58,7 @@ function SPF_isEmpty(obj) {
 }
 
 function SPF_IsItemSelected(item) {
-
-  if (item &&
+  if (SPF_CSI && item &&
       (SPF_CurrentlySelectedItem || SPF_CSI) &&
       (SPF_CurrentlySelectedItem.id == item.id|| SPF_CSI.id == item.id) ) {
     return true;
@@ -79,12 +78,7 @@ function SPF_MapYToScreenY(mapY) {
 }
 
 function SPF_IncapacitateEnemy(enemy) {
-
-  enemy._moveType = 0; // Fixed
-  enemy._walkAnime = false;
-  enemy._stepAnime = false;
-
-  enemy._isStunned = true;
+  $gameSelfSwitches.setValue([$gameMap._mapId, enemy.eventId(), 'A'], true);
 }
 
 function SPF_FindItemById(idOfItem) {
