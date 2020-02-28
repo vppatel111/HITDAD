a376bcb9771b9bf77951e0c4104c7bcfe0bb16e4
Mike Perch <mikegreberdevelopment@gmail.com>
2020-02-27 22:59:37 -0700

    Checkpoint Fix

--- HITDAD/js/plugins/SPFCheckPoint.js
+++ HITDAD/js/plugins/SPFCheckPoint.js
@@ -27,10 +27,8 @@
  */
 (function() {
 
-    let itemsAtCheckpoint = [];
-
-    let xPosition = 0;
-    let yPosition = 0;
+    let itemsAtCheckpoint = [0,0,0];
+    let itemNames = ["Milk Bottle", "Diaper Bomb", "Dad Joke"];
 
     var Game_Interpreter_pluginCommand = Game_Interpreter.prototype.pluginCommand;
     Game_Interpreter.prototype.pluginCommand = function (command, args) {
@@ -46,85 +44,36 @@
                 case "ResetEventStates":
                     resetEventSwitches();
                     break;
-                case "TopUpItems":
-                    if (args && args.length > 0)
-                    {
-                        for (let i = 0; i < args.length; ++i)
-                        {
-
-                            itemsAtCheckpoint[i] = parseInt(args[i]);
-                        }
-                    }
-                    reFillItems();
-                    break;
-                case "GainItems":
-                    GainItems(args);
-                    break;
             }
         }
     }
 
     function recordItemsAtCheckPoint() {
         let items = $gameParty.allItems();
-        // itemsAtCheckpoint = items;
-        for (let i = 0; i < items.length; ++i)
-        {
-            itemsAtCheckpoint[i] = $gameParty.numItems(items[i]);
-        }
-    }
 
-    function GainItems(args)
-    {
-        let items = $gameParty.allItems();
+        for (let i = 0; i < itemsAtCheckpoint.length; ++i) {
+            if (items[i]) {
+                for (let j = 0; j < itemNames.length; ++j) {
 
-        // let length = Math.min(items.length, args.length);
-        console.log(items);
-        for (let i = 0; i < args.length; ++i)
-        {
-            if (args[i] > 0)
-            {
-                console.log("Adding", args[i], items[i].name);
-                $gameParty.gainItem($dataItems[items[i].id], parseInt(args[i]));
+                    if (items[i].name === itemNames[j]) {
+
+                        itemsAtCheckpoint[j] = $gameParty.numItems(items[i]);
+                    }
+                }
             }
-        }
-    }
 
-    function reFillItems() {
-        let items = $gameParty.allItems();
-
-        let length = Math.min(items.length, itemsAtCheckpoint.length);
-
-        for (let i = 0; i < length; ++i)
-        {
-            let difference = itemsAtCheckpoint ? itemsAtCheckpoint[i] - $gameParty.numItems(items[i]) : 0;
-            console.log(itemsAtCheckpoint[i], $gameParty.numItems(items[i]), difference);
-            if (difference > 0)
-            {
-                console.log("Topping up", items[i].name);
-                $gameParty.gainItem($dataItems[items[i].id], difference);
-            }
         }
     }
 
     function resetToCheckpointValues() {
-        let items = $gameParty.allItems();
 
-        console.log(itemsAtCheckpoint);
-
-        let length = Math.min(items.length, itemsAtCheckpoint.length);
-
-        for (let i = 0; i < length; ++i)
+        for (let i = 0; i < itemNames.length; ++i)
         {
-            let difference = itemsAtCheckpoint ? itemsAtCheckpoint[i] - $gameParty.numItems(items[i]) : 0;
-            console.log(itemsAtCheckpoint[i], $gameParty.numItems(items[i]), difference);
-            if (difference > 0)
-            {
-                console.log("Topping up", items[i].name);
-                $gameParty.gainItem($dataItems[items[i].id], difference);
-            } else if (difference < 0)
-            {
-                console.log("Reducing", items[i].name);
-                $gameParty.loseItem($dataItems[items[i].id], -difference);
+            let difference = itemsAtCheckpoint[i] - $gameParty.numItems($dataItems[i + 1]);
+
+            if (Math.abs(difference) > 0) {
+                console.log("Topping up", $dataItems[i + 1].name);
+                $gameParty.gainItem($dataItems[i + 1], difference);
             }
         }
 
