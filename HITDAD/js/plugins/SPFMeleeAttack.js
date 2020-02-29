dd2fcfd134991bfd432f7196aa569122e70aba72
Mike Perch <mikegreberdevelopment@gmail.com>
2020-02-28 17:14:28 -0700

    Milk bottle tightened

--- HITDAD/js/plugins/SPFMeleeAttack.js
+++ HITDAD/js/plugins/SPFMeleeAttack.js
@@ -38,102 +38,131 @@
 (function() {
 
   var parameters = PluginManager.parameters('SPFMeleeAttack');
-  var attackKey = parseInt(parameters['keyCode']);
+  // var attackKey = parseInt(parameters['keyCode']);
   var itemID = parseInt(parameters['itemID']);
   let sePacify = JSON.parse(parameters['soundEffectParams'] || '{}');
   sePacify.name = parameters['soundEffect'] || '';
 
-  Input.SPFCustomKeypress = function(event) {
-    if (event.keyCode === attackKey) {  // Q
-        PerformAttack();
-    }
-  }
+  // Input.SPFCustomKeypress = function(event) {
+  //   if (event.keyCode === attackKey) {  // Q
+  //       PerformAttack();
+  //   }
+  // }
 
-  document.addEventListener('keydown', Input.SPFCustomKeypress);
+  // document.addEventListener('keydown', Input.SPFCustomKeypress);
+  //
+  // function getEnemyCollision(events) {
+  //
+  //   var enemyEvent = {};
+  //   events.forEach(function(event) {
+  //
+  //     if (event._npcType === SPF_NPCS.NORMAL_GUARD) {
+  //       enemyEvent = event;
+  //     }
+  //   });
+  //
+  //   return enemyEvent;
+  // }
 
-  function getEnemyCollision(events) {
+  // Checks if and returns an enemy that is within melee range.
+  function getEnemiesInFrontOfPlayer() {
 
-    var enemyEvent = {};
-    events.forEach(function(event) {
+    let direction = $gamePlayer.direction();
+    let closestEnemy = null;
+    let closestEnemyDistance = null;
 
-      if (event._npcType === SPF_NPCS.NORMAL_GUARD) {
-        enemyEvent = event;
+    SPF_Enemies.forEach(function(enemy) {
+      if (!enemy._npcType || SPF_IsEnemyPacified(enemy)) return;
+      let distanceToEnemy = $gamePlayer._realX - enemy._realX;
+      let verticalOffset = Math.abs($gamePlayer._realY - enemy._realY);
+
+      let forwardDistanceToEnemy = direction === 4 ?  distanceToEnemy : -distanceToEnemy;
+
+      if (forwardDistanceToEnemy < 2.0 && verticalOffset < 1.0) {
+        if (!closestEnemy || closestEnemyDistance > forwardDistanceToEnemy) {
+          closestEnemyDistance = forwardDistanceToEnemy;
+          closestEnemy = enemy;
+        }
       }
     });
 
-    return enemyEvent;
-  }
-
-  // Checks collision of player with all events based on the direction they
-  // are facing.
-  function getEventsInDirectionOfCollision() {
-
-    var direction = $gamePlayer.direction();
-
-    switch (direction) {
-      case 4: // Left
-        if ($gamePlayer.isCollidedWithEvents($gamePlayer.x - 1, $gamePlayer.y)) {
-          return $gameMap.eventsXyNt($gamePlayer.x - 1, $gamePlayer.y);
-        }
-        break;
-      case 6: // Right
-        if ($gamePlayer.isCollidedWithEvents($gamePlayer.x + 1, $gamePlayer.y)) {
-          return $gameMap.eventsXyNt($gamePlayer.x + 1, $gamePlayer.y);
-        }
-        break;
+    if (closestEnemy) {
+      SPF_IncapacitateEnemy(closestEnemy);
+      return closestEnemy;
     }
 
-    return []; // No collision with events.
+    // switch (direction) {
+    //   case 4: // Left
+    //       if ($gamePlayer._realX)
+    //     if ($gamePlayer.isCollidedWithEvents($gamePlayer.x - 1, $gamePlayer.y)) {
+    //       return $gameMap.eventsXyNt($gamePlayer.x - 1, $gamePlayer.y);
+    //     }
+    //     break;
+    //   case 6: // Right
+    //     if ($gamePlayer.isCollidedWithEvents($gamePlayer.x + 1, $gamePlayer.y)) {
+    //       return $gameMap.eventsXyNt($gamePlayer.x + 1, $gamePlayer.y);
+    //     }
+    //     break;
+    // }
+
+    // return []; // No collision with events.
 
   }
 
-  function PerformAttack() {
 
-    // Check collision between all targets.
-    var eventsHit = getEventsInDirectionOfCollision();
 
-    // Do nothing if the player didn't collide with any events.
-    if (eventsHit.length == 0) return;
-
-    var item = SPF_FindItemById(itemID);
-
-    if (!SPF_isEmpty(item) && SPF_IsItemSelected(item)) {
-
-      // Figure out if we hit an enemy.
-      var enemyHit = getEnemyCollision(eventsHit);
-
-      // Incapacitate the enemy.
-      if (!SPF_isEmpty(enemyHit)) {
-        $gameParty.loseItem(item, 1);
-        SPF_IncapacitateEnemy(enemyHit);
-        AudioManager.playSe(sePacify);
-      }
-
-    }
-
-  }
+  // function PerformAttack() {
+  //
+  //   // Check collision between all targets.
+  //   // var eventsHit = getEventsInDirectionOfCollision();
+  //   let eventHit = getEventsInDirectionOfCollision();
+  //
+  //   // Do nothing if the player didn't collide with any events.
+  //   // if (eventsHit.length == 0) return;
+  //   if (!eventHit) return;
+  //
+  //   let item = SPF_FindItemById(itemID);
+  //
+  //   if (!SPF_isEmpty(item) && SPF_IsItemSelected(item)) {
+  //
+  //     // Figure out if we hit an enemy.
+  //     var enemyHit = getEnemyCollision(eventsHit);
+  //
+  //     // Incapacitate the enemy.
+  //     if (!SPF_isEmpty(enemyHit)) {
+  //       $gameParty.loseItem(item, 1);
+  //       SPF_IncapacitateEnemy(enemyHit);
+  //       AudioManager.playSe(sePacify);
+  //     }
+  //
+  //   }
+  //
+  // }
 
   Game_Player.prototype.SPF_MeleeAttack = function() {
 
     // Check collision between all targets.
-    let eventsHit = getEventsInDirectionOfCollision();
+    // let eventsHit = getEventsInDirectionOfCollision();
+    let eventHit = getEnemiesInFrontOfPlayer();
 
     // Do nothing if the player didn't collide with any events.
-    if (eventsHit.length === 0) return;
+    // if (eventsHit.length === 0) return;
+    if (!eventHit) return;
 
     let item = SPF_FindItemById(itemID);
 
     if (!SPF_isEmpty(item) && SPF_IsItemSelected(item)) {
 
       // Figure out if we hit an enemy.
-      var enemyHit = getEnemyCollision(eventsHit);
+      // var enemyHit = getEnemyCollision(eventsHit);
+
 
       // Incapacitate the enemy.
-      if (!SPF_isEmpty(enemyHit)) {
+      // if (!SPF_isEmpty(enemyHit)) {
         $gameParty.loseItem(item, 1);
-        SPF_IncapacitateEnemy(enemyHit);
+        // SPF_IncapacitateEnemy(enemyHit);
         AudioManager.playSe(sePacify);
-      }
+      // }
 
     }
 
